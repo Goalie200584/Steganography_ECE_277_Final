6eae3a519631944fed920a9402878f669ef2317f
Goalie200584 <Goalie609@gmail.com>
2025-09-11 16:46:39 -0400

    Started debugging the None type when message has at least one space

--- dislodge_img/convert_bin_to_text.py
+++ dislodge_img/convert_bin_to_text.py
@@ -1,6 +1,5 @@
 def convert_to_text(bin_text):
     message = ""
-    print(bin_text)
     bin_text = bin_text[:-1]
     for num in bin_text:
         message += chr(int(num, 2))
--- dislodge_img/uncover_binary.py
+++ dislodge_img/uncover_binary.py
@@ -1,14 +1,23 @@
 def uncover_info(img_bin):
     current_message = ""
     bin_message = []
+    # print(img_bin) This checks out, nothing is wrong here. 
     for row in img_bin:
         for pixel in row:
             for bin in pixel:
                 if len(current_message) < 8:
                     current_message += bin[-1]
-                elif len(current_message) == 8:
+                elif len(current_message) == 8: 
+                    if current_message == "10101010":
+                        print("We've go an indicator")
+                        
                     if current_message == "10101010" and bin_message[-1] == "10101010":
                         return bin_message
+                    
                     else:
+                        # print("adding message to the bin_ ", current_message)
                         bin_message.append(current_message)
+                        if len(bin_message) == 4:
+                            print(current_message)
+                            print(bin_message)
                         current_message = f"{bin[-1]}"
