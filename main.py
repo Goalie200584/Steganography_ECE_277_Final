9e9758aae1e94ddc62b55793607d182401427dfd
Goalie200584 <Goalie609@gmail.com>
2025-09-22 08:31:29 -0400

    Cleaned up main.py

--- main.py
+++ main.py
@@ -7,24 +7,7 @@ import dislodge_img.uncover_binary as uncover_binary
 import dislodge_img.convert_bin_to_text as convert_bin_to_text
 from gui import StegApp
 import tkinter as tk
-original_img_path = "./images/image.png"
-dislodge_path = "./images/embedded_image.png"
 if __name__ == "__main__":
-    # path = int(input("Do you want to 1. embed a secret, or 2. discover a secret? "))
-    # if path == 1:
-    #     text = get_text.get_text()
-    #     text_bin, text_length = get_text.convert_text_to_binary(text)
-    #     num_of_rows, img_bin = img_to_binary.convert_img_to_binary(original_img_path, text_length)   
-    #     img_bin = embed.embed_message(text_bin, img_bin)
-    #     save_img.save_img(original_img_path, img_bin)
-    # if path == 2:
-    #     img_bin_decode = convert_to_dislodge.convert_img_to_binary(dislodge_path)
-    #     bin_text = uncover_binary.uncover_info(img_bin_decode)
-    #     decoded_message = convert_bin_to_text.convert_to_text(bin_text)
-
-    #     print(f"Decoded Message: {decoded_message}")
-
-        
     root = tk.Tk()
     app = StegApp(root)
     root.mainloop()
